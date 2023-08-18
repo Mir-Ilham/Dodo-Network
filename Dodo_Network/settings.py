d4040a9bd258d152b44278f174b10b15d39f3a9a
mir-ilham <mhmilham3@gmail.com>
2023-08-18 17:04:23 +0600

    Add api for accessing room data

--- Dodo_Network/settings.py
+++ Dodo_Network/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "base.apps.BaseConfig",
+    'rest_framework',
+    "corsheaders",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,6 +46,8 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "base.User"
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -132,3 +137,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
